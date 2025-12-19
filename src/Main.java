b6187f611fa33aeca82f41a633add58a9e1835af
Janderson <janderson.mcast@gmail.com>
2025-12-19 17:02:41 -0300

    problema troco

--- src/Main.java
+++ src/Main.java
@@ -5,27 +5,23 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		String nome1, nome2;
-		int idade1, idade2;
-		double media;
-		System.out.println("Dados da primeira pessoa");
-		System.out.printf("Nome");
-		nome1 = sc.nextLine();
-		System.out.printf("idade");
-		idade1 = sc.nextInt();
-
-		System.out.println("Dados da segunda pessoa:");
-		
-		System.out.printf("Nome");
-		sc.nextLine();
-		nome2 = sc.nextLine();
-		
-		System.out.printf("idade");
-		idade2 = sc.nextInt();
-
-		media = (double) (idade1 + idade2) / 2;
-
-		System.out.printf("A idade média de %s e %s é de %.1f anos", nome1, nome2, media);
+       double precoUnd, qtC, dinheiro, troco;
+       
+       System.out.println("Preço unitário do produto");
+       precoUnd = sc.nextDouble();
+       
+       System.out.println("Quantidade comprada");
+       qtC = sc.nextDouble();
+       
+       System.out.println("Dinheiro recebido");
+       dinheiro = sc.nextDouble();
+       
+       troco = dinheiro - (qtC * precoUnd);
+       
+       System.out.printf("Troco = %.2f%n", troco);
+       
+       
+       
 
 		sc.close();
 
