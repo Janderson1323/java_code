91edf3e3528f0ac6447cd3b7458c922a9a6ce4b7
Janderson <janderson.mcast@gmail.com>
2025-12-19 15:34:32 -0300

    Estrut sequenc Casting

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,28 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Hello Word");
-		
-		
+
+		int x, y;
+		double soma;
+
 		
 
+		System.out.println("Digite o valor de X: ");
+		x = sc.nextInt();
+		
+		System.out.println("Digite o valor de Y: ");
+		y = sc.nextInt();
+
+		soma = (double) x + y;
+
+		System.out.printf("SOMA %.2f%n", soma);
+
+		sc.close();
+		
 	}
 
 }
