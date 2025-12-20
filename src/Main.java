d9286e2cbc2daadce004424600fa2e81d812d700
Janderson <janderson.mcast@gmail.com>
2025-12-19 21:28:31 -0300

    medidas

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,33 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		
+	double a, b, c, areaQuad, areaTriang, areaTrapez;
+	
+	
+	System.out.println("Digite a medida A: ");
+	a = sc.nextDouble();
+	
+	System.out.println("Digite a medida A: ");
+	b = sc.nextDouble();
+	
+	
+	System.out.println("Digite a medida A: ");
+	c = sc.nextDouble();
+	
+	
+	areaQuad = a * a;
+	areaTriang = a * b / 2;
+	areaTrapez = ((a + b) * c) / 2;
+	
+	System.out.println(areaQuad);
+	System.out.println(areaTriang);
+	System.out.println(areaTrapez);
 
-		double distancia, combustivel, consumo;
-
-		System.out.println("Distancia percorrida");
-		distancia = sc.nextDouble();
-		System.out.println("Combustível gasto");
-		combustivel = sc.nextDouble();
-
-		consumo = distancia / combustivel;
-
-		System.out.printf("Consumo medio %.3f%n", consumo);
-
-		sc.close();
+	sc.close();
 
 	}
 
