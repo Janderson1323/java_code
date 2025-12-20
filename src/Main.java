437b9b3b0e228e146315af29a1ea44e4e5f69aca
Janderson <janderson.mcast@gmail.com>
2025-12-19 22:06:43 -0300

    Ajustes

--- src/Main.java
+++ src/Main.java
@@ -7,22 +7,23 @@ public class Main {
 		
 		
 		
-	double duracao, segundos, horas, min, seg, resto;
-	
+	double duracao, min, seg, resto;
+	int horas;
 	System.out.println("Digite a duracao em segundos: ");
 	duracao = sc.nextInt();
 	
 	resto = duracao % 3600;
 	
-	horas = duracao / 3600;
+	horas = (int)duracao / 3600;
 	min = resto / 60;
 	seg = resto % 60;
 	
 	System.out.println(horas);
+	
 	System.out.println(min);
 	System.out.println(seg);
-	System.out.println();
-	
+
+	System.out.printf("%d horas %.0f minutos %.0f segundos %n", horas, min, seg);
 
 	sc.close();
 
