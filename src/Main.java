5ff8430b65f0ada385aac14027076c944ff0c77d
Janderson <janderson.mcast@gmail.com>
2025-12-22 07:26:36 -0300

    bhaskara

--- src/Main.java
+++ src/Main.java
@@ -6,28 +6,31 @@ public class Main {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-        
-        
-		double nota1, nota2, notaFinal;
-		
-		
-		
-		System.out.printf("Digite a primeira nota:");
-		nota1 = sc.nextDouble();
-		
-		System.out.printf("Digite a segunda nota:");
-		nota2 = sc.nextDouble();
-		
-		notaFinal = nota1 + nota2;
-		
-		if (notaFinal < 60) {
-			System.out.printf("NOTA FINAL = %.1f%n", notaFinal);
-			System.out.println("REPROVADO");
+
+		double a, b, c, x1, x2, delta;
+
+		System.out.printf("Coeficiente a: ");
+		a = sc.nextDouble();
+		System.out.printf("Coeficiente b: ");
+		b = sc.nextDouble();
+		System.out.printf("Coeficiente c: ");
+		c = sc.nextDouble();
+
+		delta = (b * b) - (4 * a * c);
+
+		x1 = ((-b) + Math.sqrt(delta)) / (2 * a);
+		x2 = ((-b) - Math.sqrt(delta)) / (2 * a);
+
+		if (delta < 0) {
+			System.out.println("Esta equacao nao possui raizes reais");
 		}
 
 		else {
-			System.out.printf("NOTA FINAL = %.1f%n", notaFinal);
+			System.out.printf("X1 %.4f%n", x1);
+			System.out.printf("X2 %.4f%n", x2);
+
 		}
+
 		sc.close();
 
 	}
