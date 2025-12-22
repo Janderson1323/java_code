8a3543c1ea450cdd1fdd5554c424023593590636
Janderson <janderson.mcast@gmail.com>
2025-12-22 10:38:34 -0300

    aumento n fin

--- src/Main.java
+++ src/Main.java
@@ -7,34 +7,41 @@ public class Main {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-		double faren, celc;
-		char x;
+		double salario, newSalary, porcento, aumento;
+        int porcentagem;
+        
+		System.out.println("Digite o salario da pessoa:");
+		salario = sc.nextDouble();
+		System.out.println("Novo salario = ");
+		System.out.println("Aumento = R$ ");
+		System.out.println("Porcentagem = ");
 
-		System.out.println("Voce vai digitar a temperatura em qual escala (C/F)?");
-		x = sc.nextLine().charAt(0);
-
-		if (x == 'f') {
-
-			System.out.printf("Digite a temperatura em Fahrenheit ");
-			faren = sc.nextDouble();
-
-			celc =  5.0 / 9 * (faren - 32);
-			System.out.printf("Temperatura equivalente em Celsius %.2f", celc);
-
-		}
-		
-		else if (x == 'c') {
-			
-			System.out.printf("Digite a temperatura em Celsius ");
-			celc = sc.nextDouble();
-
-			faren = celc * 9 / 5.0  + 32.0;
-			System.out.printf("Temperatura equivalente em Fahrenheit %.2f", faren);
-			
+		if (salario <= 1000) {
+			porcentagem = 20;
+			newSalary = salario + (salario * porcentagem / 100);
 		}
 
+		else if (salario > 1000 && salario <= 3000) {
+			porcentagem = 15;
+			newSalary = salario + (salario * porcentagem / 100);
+		}
+
+		else if (salario > 3000 && salario <= 8000) {
+			porcentagem = 10;
+			newSalary = salario + (salario * porcentagem / 100);
+		}
+
+		else {
+
+			newSalary = salario + (salario * 5 / 100);
+		}
 		
+        aumento = newSalary - salario;
+        
+		System.out.printf("Novo salario %.2f%n", newSalary);
+		System.out.printf("Aumento %.2f%n", aumento);
 		
+		System.out.printf("Porcentagem %d", porcentagem);
 
 		sc.close();
 
