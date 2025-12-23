4a356335e2538d6ec89b5d8df8567a146bba883c
Janderson <janderson.mcast@gmail.com>
2025-12-23 18:37:01 -0300

    lanchonete

--- src/Main.java
+++ src/Main.java
@@ -6,31 +6,55 @@ public class Main {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
-		
-		
-		double n1, n2; 
-		
-		
 
-		System.out.println("Digite dois numeros: ");
-		
-		n1 = sc.nextDouble();
-		
-		n2 = sc.nextDouble();
-		
-		if (n1 % n2 == 0) {
-			
-			System.out.println("São multiplos");
-			
-		}
-		
-		else {
-			System.out.println("Não são multiplos");
-		}
+		double codigo, valorPagar, vlrProd;
+		int qt;
+
+		System.out.printf("Codigo do produto comprado:");
+		codigo = sc.nextDouble();
+
+		System.out.printf("Quantidade comprada:");
+		qt = sc.nextInt();
 
 		
+
+		vlrProd = 0;
 		
+		if (codigo == 1) {
+
+			vlrProd = 5.0;
+
+		}
+
+		else if (codigo == 2) {
+
+			vlrProd = 3.5;
+
+		}
+
+		else if (codigo == 3) {
+
+			vlrProd = 4.80;
+
+		}
+
+		else if (codigo == 4) {
+
+			vlrProd = 8.90;
+
+		}
+
+		else if (codigo == 5) {
+
+			vlrProd = 7.32;
+
+		}
+		
+		valorPagar = vlrProd * qt;
+		
+		
+		System.out.printf("Valor a pagar: %.2f%n", valorPagar);
+
 		sc.close();
 
 	}
