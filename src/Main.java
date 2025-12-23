33432a0d6694c8fb52c33e66542b19b9a397cdd1
Janderson <janderson.mcast@gmail.com>
2025-12-23 18:25:08 -0300

    multiplos

--- src/Main.java
+++ src/Main.java
@@ -6,47 +6,31 @@ public class Main {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-
-		double salario, newSalary, porcento, aumento;
-        int porcentagem;
-        
-		System.out.println("Digite o salario da pessoa:");
-		salario = sc.nextDouble();
-		System.out.println("Novo salario = ");
-		System.out.println("Aumento = R$ ");
-		System.out.println("Porcentagem = ");
-
 		
 		
-		porcentagem = 0;
 		
-		if (salario <= 1000) {
-			porcentagem = 20;
-			newSalary = salario + (salario * porcentagem / 100);
-		}
+		double n1, n2; 
+		
+		
 
-		else if (salario > 1000 && salario <= 3000) {
-			porcentagem = 15;
-			newSalary = salario + (salario * porcentagem / 100);
+		System.out.println("Digite dois numeros: ");
+		
+		n1 = sc.nextDouble();
+		
+		n2 = sc.nextDouble();
+		
+		if (n1 % n2 == 0) {
+			
+			System.out.println("São multiplos");
+			
 		}
-
-		else if (salario > 3000 && salario <= 8000) {
-			porcentagem = 10;
-			newSalary = salario + (salario * porcentagem / 100);
-		}
-
+		
 		else {
-            porcentagem = 5;
-			newSalary = salario + (salario * 5 / 100);
+			System.out.println("Não são multiplos");
 		}
-		
-        aumento = newSalary - salario;
-        
-		System.out.printf("Novo salario %.2f%n", newSalary);
-		System.out.printf("Aumento %.2f%n", aumento);
-		
-		System.out.printf("Porcentagem %d", porcentagem);
 
+		
+		
 		sc.close();
 
 	}
