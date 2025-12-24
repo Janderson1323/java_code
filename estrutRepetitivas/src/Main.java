4d174d250f4346763a3bd5cd2326f5e7ae4905f1
Janderson <janderson.mcast@gmail.com>
2025-12-24 20:50:04 -0300

    Pares consecutivos

--- estrutRepetitivas/src/Main.java
+++ estrutRepetitivas/src/Main.java
@@ -6,52 +6,24 @@ public class Main {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		
-		
-		double codigo, gas, alcool, diesel;
-		
 
-		System.out.println("Informe um codigo (1, 2, 3) ou 4 para parar:");
-		codigo = sc.nextDouble();
-		
-		
-		gas = 0;
-		alcool = 0;
-		diesel = 0;
-		while (codigo != 4) {
-			
-			
-			
-			System.out.println("Informe um codigo (1, 2, 3) ou 4 para parar:");
-			codigo = sc.nextInt();
-			
-			if (codigo == 1) {  
-				alcool += 1;
+		double n, soma;
+		System.out.println("Digite um numero inteiro:");
+		n = sc.nextDouble();
+
+		while (n != 0) {
+			if (n % 2 == 0) {
+				soma = n + (n + 2) + (n + 4) + (n + 6) + (n + 8);
+			} else {
+				n += 1;
+				soma = n + (n + 2) + (n + 4) + (n + 6) + (n + 8);
 			}
-			
-			else if (codigo == 2) {
-				gas += 1;
-			}
-			
-			else if (codigo == 3) {
-				diesel += 1;
-			}
-			
-			if (codigo == 4) {
-				System.out.println("MUITO OBRIGADO");
-				System.out.printf("Alcool %.2f%n", alcool);
-				System.out.printf("Gasolina %.2f%n", gas);
-				System.out.printf("Diesel %.2f%n", diesel);
-				
-			}
-			
-			
-			
-			
-			
+
+			System.out.printf("SOMA = %.2f%n", soma);
+			System.out.println("Digite um numero inteiro ");
+			n = sc.nextDouble();
+
 		}
-		
-		
 
 		sc.close();
 
