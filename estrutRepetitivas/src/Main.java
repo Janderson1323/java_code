eb99a55fb061ba5454f280daae4e95a0773b411a
Janderson <janderson.mcast@gmail.com>
2025-12-24 16:33:14 -0300

    Senha fixa

--- estrutRepetitivas/src/Main.java
+++ estrutRepetitivas/src/Main.java
@@ -6,39 +6,17 @@ public class Main {
 
 		Scanner sc = new Scanner(System.in);
 
-		double x, soma, media;
-		int i;
+		double senha;
 
-		System.out.println("Digite as idades: ");
-		x = sc.nextDouble();
+		System.out.println("Digite a senha: ");
+		senha = sc.nextDouble();
 		
-
-		if (x < 0) {
-			
-			System.out.println("IMPOSSIVEL CALCULAR");
+		while (senha != 2002) {
+			System.out.printf("Senha Invalida! Tente novamente: ");
+			senha = sc.nextDouble();
 		}
-
-		else {
-			soma = 0;
-			i = 0;
-			
-			
-			
-			while (x >= 0) {
-				i++;
-				soma = soma + x;
-				x = sc.nextDouble();
-
-			}
-			
-			media = soma / i;
-			System.out.printf("MEDIA = %.2f%n", media);
-
-		}
-
 		
-
-		
+		System.out.println("Acesso permitido");
 
 	}
 
